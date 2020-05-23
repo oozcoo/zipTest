e95f1d94e31ae021ede937cfb10e917de87a8976
oozcoo <oozcoo@blueark.com>
2020-05-23 21:05:37 +0900

    master

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+        WISetup wi = new WISetup();
+        wi.Run();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/WISetup.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using Ionic.Zip;
+using System.IO;
+// using Ada.Web;
+
+public class WISetup
+{
+    private static int retry = 0;
+
+    public void Run() {
+        try {
+            string srcPath = Path.Combine(Application.streamingAssetsPath, "wi.zip");
+            string destPath = Path.Combine(Application.persistentDataPath, "wi");
+
+            if (Directory.Exists(destPath)) {
+                Directory.Delete(destPath, true);
+            }
+
+            if (!Directory.Exists(destPath)) {
+                Directory.CreateDirectory(destPath);
+            }
+
+#if UNITY_IOS || UNITY_EDITOR
+            srcPath = "file://" + srcPath;
+#endif            
+
+            UnityEngine.Networking.UnityWebRequest www = UnityEngine.Networking.UnityWebRequest.Get(srcPath);
+            www.SendWebRequest();
+            while (!www.isDone) {}
+
+            byte[] results = www.downloadHandler.data;
+            using (var ms = new MemoryStream(results)) {
+                using (ZipFile zip = ZipFile.Read(ms)) {
+                    zip.ExtractAll(destPath, ExtractExistingFileAction.OverwriteSilently);
+                }
+            }
+            retry = 0;
+            Debug.Log("Web Interface - Setup Success");
+        }
+        catch (System.Exception e) {
+            retry++;
+            Debug.LogError("Web Interface - Setup fail " + e);
+
+            if (retry < 3) {
+                Run();
+                Debug.Log("Web Interface - Retry Web Interface Setup");
+            }
+        }
+    }
+}
